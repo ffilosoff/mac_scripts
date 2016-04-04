6796ba3e8d6284f6790ec07938b9259fbf996943
ffilosoff <appanpotemkin@gmail.com>
2016-04-04 20:57:24 +0300

    fixed_endless_cycle_if_failure

--- ttl.sh
+++ ttl.sh
@@ -1,17 +1,25 @@
 #! /bin/bash
-echo `date +"%D %T"`>> /Users/pav/logs/ttl.log
+logfile=/Users/pav/logs/ttl.log 
+echo `date +"%D %T"`>> $logfile 
+i=1
 while true; do
 	sudo /usr/sbin/sysctl -w net.inet.ip.ttl=65
 	ttl=`sudo /usr/sbin/sysctl net.inet.ip.ttl`
 	echo $ttl
 	len=${#ttl}-1
 	last_char=${ttl:len:1}
-	echo -n `date +"%D %T"`>> /Users/pav/logs/ttl.log
+	echo `date +"%D %T"` "Trying to change TTL ($i)" >> $logfile
+	echo -n `date +"%D %T"`>> $logfile
 	if [ $last_char = "5" ]; then
-		echo " TTL changed" >> /Users/pav/logs/ttl.log
+		echo " Success. TTL changed" >> $logfile
 		break
 	else
-		echo " TTL not changed" >> /Users/pav/logs/ttl.log
+		echo " TTL not changed" >> $logfile
 		sleep 2s
 	fi
+	if [ $i -eq 3 ]; then
+		echo " Falure. TTL not changed" >> $logfile
+		break;
+	fi
+	let i+=1
 done 
